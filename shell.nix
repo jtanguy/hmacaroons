3e886a657890184823722fbcbf126bfa4a0f0404
Julien Tanguy <julien.tanguy@jhome.fr>
2015-09-05 01:53:08 +0200

    Import generic, updated shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
 let hspkgs = pkgs.haskell.packages.${compiler}.override {
-     overrides = self: super: {
-       hmacaroons = self.callPackage ./. {};
-      };
-   };
+  overrides = self: super: {
+    this = self.callPackage ./. {};
+  };
+};
+  hsWithTools = pkgs.lib.overrideDerivation hspkgs.this (attrs: with pkgs; {
+      buildInputs = [ /* Other deps */ ] ++ attrs.buildInputs;
+      });
 in
-  hspkgs.hmacaroons.env
+  hsWithTools.env
